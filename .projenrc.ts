78bbe165b21837ce49601193f9ddb96073d59089
Matt Lewis <hello@mattlewis.me>
2023-01-10 19:33:49 +0000

    fix: ignore node from renovate updates

--- .projenrc.ts
+++ .projenrc.ts
@@ -66,6 +66,10 @@ const project = new cdk.JsiiProject({
   renovatebotOptions: {
     scheduleInterval: ['before 1am on Monday'],
     ignoreProjen: false,
+    ignore: [
+      // managed by projen
+      'node',
+    ],
     overrideConfig: {
       rangeStrategy: 'bump',
       extends: ['config:base', 'group:allNonMajor', 'group:recommended', 'group:monorepos'],
