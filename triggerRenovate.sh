f370cdc8f27cc7b43ff2affc74d083bd3afc6adc
Matt Lewis <hello@mattlewis.me>
2023-01-12 12:41:00 +0000

    fix(renovate): rename and fix upgrade trigger script

--- triggerRenovate.sh
+++ triggerRenovate.sh
@@ -16,22 +16,22 @@ function testGhCli() {
 }
 
 # Finds all repos with a given suffix under REPO_OWNER org, then iterates over
-# them triggering an `upgrade-main` workflow for each. 
+# them triggering a `renovate` workflow for each.
 function runUpgradeMain() {
     local owner="${REPO_OWNER}";
     local repos=($(gh repo list "${owner}" --limit "${REPO_LIMIT}" --no-archived --json name -q ".[].name | select(endswith(\"${REPO_SUFFIX}\"))"));
     RC=0; # return code counts total number of failures
     for repo in ${repos[@]}; do
-        echo "Triggering upgrade-main workflow on repo ${repo}";
-        if gh workflow run --repo "${owner}/${repo}" --ref main upgrade-main; then
+        echo "Triggering renovate workflow on repo ${repo}";
+        if gh workflow run --repo "${owner}/${repo}" --ref main renovate.yml; then
             sleep 2; # Give GitHub enough time to register the workflow_dispatch event
             echo 'Verifying workflow_dispatch event was actually created...';
-            local runningCount=$(gh run list --repo "${owner}/${repo}" --workflow=upgrade-main.yml --json 'name,status' -q '.[0] | select((.status=="in_progress") or (.status=="queued"))' | wc -l);
-            test "${runningCount}" -eq 0 && echo -e "${RED}Could not find trigger for upgrade-main workflow on repo ${repo}.${NC}\n" && RC=$(($RC + 1)) \
+            local runningCount=$(gh run list --repo "${owner}/${repo}" --workflow=renovate.yml --json 'name,status' -q '.[0] | select((.status=="in_progress") or (.status=="queued"))' | wc -l);
+            test "${runningCount}" -eq 0 && echo -e "${RED}Could not find trigger for renovate workflow on repo ${repo}.${NC}\n" && RC=$(($RC + 1)) \
                 || echo -e "${GREEN}Trigger succeeded. Continuing...${NC}\n";
         else
             RC=$(($RC + 1));
-            echo -e "${RED}Trigger for upgrade-main workflow on repo ${repo} failed.${NC}\n";
+            echo -e "${RED}Trigger for renovate workflow on repo ${repo} failed.${NC}\n";
         fi
     done
     echo -e "\nTotal failure count is: ${RC}";
