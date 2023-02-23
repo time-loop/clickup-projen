693fe66e0b7f8b52236f6199065ae3c37845f0f4
Andrew Hammond <445764+ahammond@users.noreply.github.com>
2023-02-23 10:46:02 -0800

    chore(deps): bump min node version to 14.21.3 (#118)

--- src/clickup-ts.ts
+++ src/clickup-ts.ts
@@ -56,8 +56,8 @@ export module clickupTs {
     ],
     gitignore: ['/.npmrc', '.idea', '.yalc', 'yalc.lock'],
 
-    minNodeVersion: '14.18.0', // Required by eslint-import-resolver-typescript@3.3.0
-    workflowNodeVersion: '14.18.0',
+    minNodeVersion: '14.21.3', // Required by eslint-import-resolver-typescript@3.3.0
+    workflowNodeVersion: '14.21.3',
 
     prettier: true,
     prettierOptions: {
