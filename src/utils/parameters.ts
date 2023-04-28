66ae032218c0724983b6d85963d163b2197891a6
jeisson-clickup <116575075+jeisson-clickup@users.noreply.github.com>
2023-04-28 15:48:07 -0500

    chore(infra): upgrading node version to 16.20.0 [CLK-239059] (#144)
    * chore(infra): upgrading node version to 16.20.0
    
    * chore: self mutation
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    * updating yarn lock
    
    * chore: removing nnecesary comment
    
    * chore: self mutation
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    * setting up PROJEN_NODE_VERSION in 16.20.0
    
    * deleting nodeversion from .projenrc.ts file
    
    * chore: adding PROJEN_NODE_VERSION in cdk-diff
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: rfernandez-clickup <115831691+rfernandez-clickup@users.noreply.github.com>

--- src/utils/parameters.ts
+++ src/utils/parameters.ts
@@ -1,4 +1,4 @@
 export class parameters {
   static PROJEN_MIN_ENGINE_NODE_VERSION: string = '14.18.0';
-  static PROJEN_NODE_VERSION: string = '14.21.3';
+  static PROJEN_NODE_VERSION: string = '16.20.0';
 }
