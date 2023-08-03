ee6fddad49be26ecc26e5d5d12441c5daf63baf9
Andrew Hammond <445764+ahammond@users.noreply.github.com>
2023-08-03 15:23:11 -0700

    feat(update-projen): separate update projen wf [CLK-273691] (#196)
    Co-authored-by: github-actions <github-actions@github.com>

---
+++ test/update-projen.test.ts
@@ -0,0 +1,16 @@
+import { typescript, Testing } from 'projen';
+
+import { updateProjen } from '../src/update-projen';
+
+describe('addAddToProjectWorkflowYml', () => {
+  test('file added', () => {
+    const project = new typescript.TypeScriptProject({
+      defaultReleaseBranch: 'main',
+      name: 'test',
+    });
+    updateProjen.addWorkflows(project);
+    const synth = Testing.synth(project);
+    expect(synth['.github/workflows/add-to-update-projen-project.yml']).toMatchSnapshot();
+    expect(synth['.github/workflows/upgrade-projen-main.yml']).toMatchSnapshot();
+  });
+});
