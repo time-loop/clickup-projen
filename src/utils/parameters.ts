ff47a6b8a96ce6374fd60bdf25d1204ab745e371
Andrew Hammond <445764+ahammond@users.noreply.github.com>
2023-08-24 13:55:18 -0700

    feat: use node18 [CLK-323868]

--- src/utils/parameters.ts
+++ src/utils/parameters.ts
@@ -1,4 +1,4 @@
 export class parameters {
   static PROJEN_MIN_ENGINE_NODE_VERSION: string = '14.18.0';
-  static PROJEN_NODE_VERSION: string = '16.20.2';
+  static PROJEN_NODE_VERSION: string = '18.17.1';
 }
